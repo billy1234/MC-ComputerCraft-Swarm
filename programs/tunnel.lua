ba757031dc788db2e051f0a359691a1224f5bf3b
Billy <billy@example.com>
2025-04-22 13:48:34 +1000

    fixed imports

--- programs/tunnel.lua
+++ programs/tunnel.lua
@@ -1,9 +1,9 @@
-os.loadAPI("apis/data")
-os.loadAPI("apis/base")
-os.loadAPI("apis/movement")
+os.loadAPI("apis/data.lua")
+os.loadAPI("apis/base.lua")
+os.loadAPI("apis/movement.lua")
 
 
-VERSION = "0.0.0"
+VERSION = "0.0.1"
 
 --This function assumes you will start from inside the tunnel cover (0,0,0) in local cords
 local function findTunnelEnterance()
