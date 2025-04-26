084bb69fbc24c244a83e588e81cc5a9a89b391ff
Billy <billy@example.com>
2025-04-26 11:37:57 +1000

    added blocking of tunnel direction config

--- programs/tunnel.lua
+++ programs/tunnel.lua
@@ -3,7 +3,14 @@ os.loadAPI("apis/base.lua")
 os.loadAPI("apis/movement.lua")
 
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
+
+--allow W/ N /S in test world
+ALLOWED_DIRECTIONS = {
+    [movement.ORIENTATIONS.west] = true,
+    [movement.ORIENTATIONS.north] = true,
+    [movement.ORIENTATIONS.south] = true,
+}
 
 --This function assumes you will start from inside the tunnel cover (0,0,0) in local cords
 local function findTunnelEnterance()
@@ -11,6 +18,12 @@ local function findTunnelEnterance()
         movement.MOVEMENTS.down,
         movement.MOVEMENTS.up,
     })
+
+    if(ALLOWED_DIRECTIONS[movement.ORIENTATIONS.east]) then
+        print("East allowed")
+    else
+        print("East not allowed")
+    end
 
     print(moves:doNext(turtle))
     print(moves:doNext(turtle))
